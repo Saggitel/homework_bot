9adf0d57cff0308f0660165e4d07e3938cc02ef4
Saggitel <lazba.timur@gmail.com>
2022-09-19 08:35:55 +0300

    pep8

--- homework.py
+++ homework.py
@@ -88,7 +88,9 @@ def parse_status(homework):
     if homework_status not in HOMEWORK_STATUSES:
         raise KeyError('Неизвестный статус домашней работы')
     if 'homework_name' not in homework:
-        raise KeyError('Отсутствуют ключ "homework_name" : homework = {homework}.')
+        raise KeyError(
+            'Отсутствуют ключ "homework_name" : homework = {homework}.'
+    )
 
     verdict = HOMEWORK_STATUSES[homework_status]
 
