51f635f6ed5836faa5ffb3ec1555766dfaf67445
Saggitel <lazba.timur@gmail.com>
2022-09-30 14:00:49 +0300

    Правки

--- homework.py
+++ homework.py
@@ -47,6 +47,7 @@ def get_api_answer(current_timestamp):
     """Отправка запроса к сайту Яндекс Практикум."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
+    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
     try:
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
         logging.info(response)
